513a1d96365b69f8808594b88f0676b2b7646b63
TuoKy <tuomas.kytta@gmail.com>
2016-01-30 12:34:53 +0200

    basic game with multiplayer, has many bugs

---
+++ Knockout Arena/Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CameraController : MonoBehaviour
+{
+
+    public int Speed;
+    public Transform cameraParent;
+
+    private Vector2 oldMousePos;
+    private float zoomlvl = 0.5f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * Time.deltaTime * Speed;
+        cameraParent.Translate(movement, Space.Self);
+
+        if (Input.GetMouseButton(1))
+        {
+            cameraParent.Rotate(0, oldMousePos.x - Input.mousePosition.x, 0);
+        }
+        oldMousePos = Input.mousePosition;
+
+
+
+    }
+
+    public void setSpeed(int value)
+    {
+        Speed = value;
+    }
+
+    public void setPosition(Transform kohde)
+    {
+        cameraParent.position = kohde.position;
+    }
+
+}
