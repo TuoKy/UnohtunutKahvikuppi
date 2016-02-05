206a39e7071ef27c2779d06b4e5b75eefd5f0a25
ArttuHeinonen <arttu.hei@gmail.com>
2016-02-05 08:46:45 +0200

    MainMenu functional & Test build to build folder
    - Game boots to MainMenu scene
    - Start & Exit buttons work
    - Exit works in editor and in build. Not in WebPlayer.

---
+++ Knockout Arena/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public static MainMenuManager Instance { get; private set; }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ArenaScene");
+    }
+
+    public void OptionsScreen()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
