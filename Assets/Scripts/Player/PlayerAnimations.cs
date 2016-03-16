fd97ce8441277180d205949202d0953c4ddc5f43
ArttuHeinonen <H3425@LABRANET.JAMK.FI>
2016-03-16 14:20:53 +0200

    Smoother animation & blocking

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -10,12 +10,6 @@ public class PlayerAnimations : NetworkBehaviour
     //For attacks
     public GameObject attackHitboxLeftHand;
     public GameObject attackHitboxRightHand;
-    public Text debugText;
-	
-    void Start()
-    {
-        debugText = GameObject.Find("debugText").GetComponent<Text>();
-    }
 
     [Command]
     public void CmdSetTrigger(string trigger)
