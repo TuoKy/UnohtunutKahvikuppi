e788f8a55a70c1a65c1ecbfe4ce70b77cedb48f1
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2016-04-13 11:52:53 +0300

    Hitsausta

--- Assets/Scripts/MenuScripts/NetworkLobbyHook.cs
+++ Assets/Scripts/MenuScripts/NetworkLobbyHook.cs
@@ -7,12 +7,17 @@ public class NetworkLobbyHook : LobbyHook
 {
     public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
     {
+        base.OnLobbyServerSceneLoadedForPlayer(manager, lobbyPlayer, gamePlayer);
+        
+        /*
         LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        
         NetworkSpaceship spaceship = gamePlayer.GetComponent<NetworkSpaceship>();
 
         spaceship.name = lobby.name;
         spaceship.color = lobby.playerColor;
         spaceship.score = 0;
         spaceship.lifeCount = 3;
+        */
     }
 }
