2e85ffe437d6bb1d38dee701bff0aa79c737cca1
AkiPylvalainen <H3187@student.jamk.fi>
2016-04-26 23:46:40 +0300

    Eight stalactites moving up and down

---
+++ Assets/Scripts/SceneScripts/RoundaboutArena/UpAndDown.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class UpAndDown : NetworkBehaviour
+{
+    private float verticalSpeed = 0.2f;
+
+    private float maxY = 6;
+    public bool movingUp;
+
+    [ClientCallback]
+	void FixedUpdate () {
+        MoveUpAndDown();
+	}
+
+    private void MoveUpAndDown()
+    {
+        if (movingUp)
+        {
+            if (gameObject.transform.position.y < maxY)   // Up
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y + verticalSpeed), gameObject.transform.position.z), verticalSpeed);
+            }
+            else
+            {
+                movingUp = false;
+            }
+        }
+        else
+        {
+            if (gameObject.transform.position.y > -maxY)   // Down
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y - verticalSpeed), gameObject.transform.position.z), verticalSpeed);
+            }
+            else
+            {
+                movingUp = true;
+            }
+        }
+    }
+}
