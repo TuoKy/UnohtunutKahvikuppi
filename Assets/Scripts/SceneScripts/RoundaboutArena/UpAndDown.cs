51277555e0cb8d3024c539e9311e2b11d8f54de3
TuoKy <H3408@LABRANET.JAMK.FI>
2016-04-27 12:29:52 +0300

    asd

--- Assets/Scripts/SceneScripts/RoundaboutArena/UpAndDown.cs
+++ Assets/Scripts/SceneScripts/RoundaboutArena/UpAndDown.cs
@@ -6,38 +6,45 @@ using UnityEngine.Networking;
 public class UpAndDown : NetworkBehaviour
 {
     private float verticalSpeed = 0.2f;
+    public List<GameObject> platform;
 
     private float maxY = 5.5f;
-    public bool movingUp;
+    public bool movingUp = false;
 
-    [ClientCallback]
-	void FixedUpdate () {
-        MoveUpAndDown();
+    [ServerCallback]
+    void Start()
+    {
+        RpcFuckTheShit();
+    }
+
+
+    [ClientRpc]
+	void RpcFuckTheShit () {
+        StartCoroutine(moveUp());
 	}
 
     private void MoveUpAndDown()
     {
-        if (movingUp)
-        {
-            if (gameObject.transform.position.y < maxY)   // Up
-            {
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y + verticalSpeed), gameObject.transform.position.z), verticalSpeed);
-            }
-            else
-            {
-                movingUp = false;
-            }
-        }
-        else
-        {
-            if (gameObject.transform.position.y > -maxY)   // Down
-            {
-                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y - verticalSpeed), gameObject.transform.position.z), verticalSpeed);
-            }
-            else
-            {
-                movingUp = true;
-            }
-        }
+          
     }
+
+    IEnumerator moveUp()
+    {
+        foreach (GameObject plat in platform)
+        {
+            plat.transform.position = Vector3.Lerp(plat.transform.position, plat.transform.position + new Vector3(0, 5, 0), 15f);
+        }
+        yield return new WaitForSeconds(3f);
+        
+    }
+
+    IEnumerator moveDown()
+    {
+        foreach (GameObject plat in platform)
+        {
+            plat.transform.position = Vector3.Lerp(plat.transform.position, plat.transform.position - new Vector3(0, 10, 0), 15f);
+        }
+        yield return new WaitForSeconds(3f);
+    }
+
 }
